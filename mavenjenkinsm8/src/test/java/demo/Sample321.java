6d56660287b8677dcb119c149cbfc7a909f82714
rajar <rajar@Raja_Ranjan>
2026-05-27 11:26:19 +0530

    Initial commit

---
+++ mavenjenkinsm8/src/test/java/demo/Sample321.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Sample321 {
+
+		@Test
+		public void createSample1Test() {
+
+			System.out.println("I am sample Test 1 from Sample321Test");
+		}
+
+		@Test
+		public void createSample2Test() {
+
+			System.out.println("I am sample Test 2 from sample321Test");
+		}
+
+		@Test
+		public void createSample3Test() {
+
+			System.out.println("I am sample Test 3 from Sample321Test");
+		}
+	}
+
+
---
+++ mavenjenkinsm8/src/test/java/sample/Demo11Test.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo11Test {
+
+	@Test
+	public void createDemo1Test() {
+		
+		System.out.println("I am Demo Test 1 from Demo11Test");
+	}
+	
+	@Test
+	public void createDemo2Test() {
+		
+		System.out.println("I am Demo Test 2 from Demo11Test");
+	}
+	
+	@Test
+	public void createDemo3Test() {
+		
+		System.out.println("I am Demo Test 3 from Demo11Test");
+	}
+}
